cae96ac789459e0cf76552138044299d4d2c0d12
ipfsnut <dylan@wippublishing.com>
2025-03-10 18:56:46 -0500

    trading?

---
+++ netlify/functions/trade.js
@@ -0,0 +1,17 @@
+exports.handler = async function(event) {
+  const { amount, fid } = JSON.parse(event.body);
+  
+  // Get Base PAGE token config
+  const basePageToken = PAGE_TOKEN_CONFIG.find(token => token.chainId === 8453);
+  
+  // Build Uniswap URL with pre-filled values
+  const swapUrl = `https://app.uniswap.org/#/swap?exactField=input&exactAmount=${amount}&inputCurrency=ETH&outputCurrency=${basePageToken.address}&chain=base`;
+  
+  return {
+    statusCode: 302,
+    headers: {
+      Location: swapUrl,
+      'Cache-Control': 'no-cache'
+    }
+  };
+}
