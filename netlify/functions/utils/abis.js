015ab327030b2ec6e1f03ad0918f88eccef61eed
Dylan <40326908+ipfsnut@users.noreply.github.com>
2025-03-13 22:30:20 -0500

    Update abis.js

--- netlify/functions/utils/abis.js
+++ netlify/functions/utils/abis.js
@@ -60,3 +60,81 @@ const UNISWAP_V2_PAIR_ABI = [
 module.exports = {
   UNISWAP_V2_PAIR_ABI
 };
+
+// Uniswap V3 Pool ABI (minimal for getting slot0 and token addresses)
+const UNISWAP_V3_POOL_ABI = [
+  {
+    "inputs": [],
+    "name": "slot0",
+    "outputs": [
+      {
+        "internalType": "uint160",
+        "name": "sqrtPriceX96",
+        "type": "uint160"
+      },
+      {
+        "internalType": "int24",
+        "name": "tick",
+        "type": "int24"
+      },
+      {
+        "internalType": "uint16",
+        "name": "observationIndex",
+        "type": "uint16"
+      },
+      {
+        "internalType": "uint16",
+        "name": "observationCardinality",
+        "type": "uint16"
+      },
+      {
+        "internalType": "uint16",
+        "name": "observationCardinalityNext",
+        "type": "uint16"
+      },
+      {
+        "internalType": "uint8",
+        "name": "feeProtocol",
+        "type": "uint8"
+      },
+      {
+        "internalType": "bool",
+        "name": "unlocked",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "token0",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "token1",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  }
+];
+
+module.exports = {
+  UNISWAP_V2_PAIR_ABI,
+  UNISWAP_V3_POOL_ABI
+};
